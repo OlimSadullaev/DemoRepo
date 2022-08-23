f678c272450736a84398b2ab0f9f763289034856
OlimSadullaev <91972198+OlimSadullaev@users.noreply.github.com>
2022-08-23 16:09:07 +0500

    Add files via upload
    changes added

---
+++ EFCoreRelationship/Character.cs
@@ -0,0 +1,15 @@
+﻿namespace EFCoreRelationship
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string RgpClass { get; set; } = string.Empty;
+
+        public User User { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ EFCoreRelationship/User.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreRelationship
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public List<Character> Characters { get; set; }
+    }
+}
